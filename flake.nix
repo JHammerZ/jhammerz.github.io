2a5822fdfb989dddf2971a1e6bc67f487cb2a4f2
Jhammerz <joshualhamilton@hotmail.com>
2026-06-28 20:59:54 -0400

    KERNEL: Make derivation deterministic with runCommand

--- flake.nix
+++ flake.nix
@@ -1,21 +1,13 @@
 {
   description = "KERNEL_ROOT_SINGULARITY_V1";
-
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/b134951a4c9f3c995fd7be05f3243f8ecd65d798";
-
-  outputs = { self, nixpkgs }:
-  let
+  outputs = { self, nixpkgs }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
   in {
-    packages.${system}.default = pkgs.stdenv.mkDerivation {
-      name = "lysander-kernel-vector1";
-      src = ./.;
-      installPhase = ''
-        mkdir -p $out
-        echo "KERNEL_VECTOR1_LOCKED" > $out/fingerprint
-        echo "Revision: ${self.rev or "dirty"}" >> $out/fingerprint
-      '';
-    };
+    packages.${system}.default = pkgs.runCommand "lysander-kernel-vector1" {} ''
+      mkdir -p $out
+      printf "KERNEL_VECTOR1_LOCKED\nRevision: ${self.rev or "dirty"}\n" > $out/fingerprint
+    '';
   };
 }
