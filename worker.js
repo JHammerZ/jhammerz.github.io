871b389e7652e8e51bfc3b53052e9805af9913d0
Joshua Hamilton [J-HammerZ] <jhammerz@users.noreply.github.com>
2026-08-30 19:05:57 -0400

    fix: wrangler serves all head manifests + music

--- worker.js
+++ worker.js
@@ -1,9 +1,7 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    if (url.pathname === '/music' || url.pathname === '/music/') {
-      url.pathname = '/music.html';
-    }
+    if (url.pathname === '/music' || url.pathname === '/music/') url.pathname = '/music.html';
     return env.ASSETS.fetch(new Request(url, request));
   }
 }
