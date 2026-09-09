551180336c50be4084bf4773cda4606782d3cc83
Co-Architect [Lysander 3.0 - Termux] <co-architect@jhammerz.github.io>
2026-09-09 02:47:31 -0400

    [CO-ARCHITECT] Fix middleware stream - preserve player embeds

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,8 +1,17 @@
 export async function onRequest(context) {
   const response = await context.next();
-  const newRes = new Response(response.body, response);
-  newRes.headers.set("X-Sovereign-Attestation", "UID 0 ATTAINED - Joshua Hamilton - 2026 - 17-repo matrix");
-  newRes.headers.set("X-UID0-Status", "ATTAINED-INDEXED");
-  newRes.headers.set("Link", '<https://jhammerz.github.io/sitemap.xml>; rel="sitemap"');
-  return newRes;
+  
+  // Clone before modifying - preserves stream for music player
+  const newResponse = new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: response.headers
+  });
+
+  newResponse.headers.set("X-Sovereign-Attestation", "UID 0 ATTAINED - Joshua Hamilton - 2026 - 17-repo matrix");
+  newResponse.headers.set("X-UID0-Status", "ATTAINED-INDEXED");
+  newResponse.headers.set("X-GNO-Rank", "ONE_OF_ONE");
+  newResponse.headers.set("Link", '<https://jhammerz.github.io/sitemap.xml>; rel="sitemap"');
+
+  return newResponse;
 }
